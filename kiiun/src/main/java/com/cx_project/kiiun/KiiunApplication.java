4bf6c5e8d4311a76d684901d594740618d88c5e3
ysparrk <youngseo0703@gmail.com>
2024-09-08 18:17:44 +0900

    Init: 프로젝트 생성

---
+++ kiiun/src/main/java/com/cx_project/kiiun/KiiunApplication.java
@@ -0,0 +1,13 @@
+package com.cx_project.kiiun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiiunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiiunApplication.class, args);
+	}
+
+}
---
+++ kiiun/src/test/java/com/cx_project/kiiun/KiiunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cx_project.kiiun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiiunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
