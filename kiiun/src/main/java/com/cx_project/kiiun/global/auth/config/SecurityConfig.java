f64e4bce75e89bf9f5b8ed9117f0a54c095e3a89
ysparrk <youngseo0703@gmail.com>
2024-09-12 18:39:14 +0900

    fix: cors 가능한 url 추가

--- kiiun/src/main/java/com/cx_project/kiiun/global/auth/config/SecurityConfig.java
+++ kiiun/src/main/java/com/cx_project/kiiun/global/auth/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:1234"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
